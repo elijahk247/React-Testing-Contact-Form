5a02ec9c6d549218ad5a7553490798faaeea3ad3
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-09-07 14:32:53 -0400

    Fixed issue: should be testing in app.test.js, instead of testing file that i made

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+
 import App from "./App";
+import ContactForm from './components/ContactForm'
 
 test("renders App without crashing", () => {
   render(<App />);
 
   // act
   // if App renders without crashing, should be able to grab the div className='App'
-  const container = screen.getByTestId(/app/i)
+  const container = screen.getByTestId(/renderapp/i)
 
   // assert
   // check if the container (div class app) is in the document 
   expect(container).toBeInTheDocument()
 });
+
+test('Test the form inputs', () => {
+  render(<ContactForm />)
+
+  const firstName = screen.getByText(/first name/i)
+})
